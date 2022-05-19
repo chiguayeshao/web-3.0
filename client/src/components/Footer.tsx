d38adca670008355e474e7d344d95e6af8efffd1
gem <chiguayeshao@gmail.com>
2022-05-19 12:53:14 +0800

    uodate details

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -29,14 +29,14 @@ const Footer = () => (
                 Come join us and hear for the unexpected miracle
             </p>
             <p className="text-white text-sm text-center font-medium mt-2">
-                info@kryptomastery.com
+                xiaotian@kryptomastery.com
             </p>
         </div>
 
         <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
         <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-            <p className="text-white text-left text-xs">@kryptomastery2022</p>
+            <p className="text-white text-left text-xs">@FuckKrypto2022</p>
             <p className="text-white text-right text-xs">All rights reserved</p>
         </div>
     </div>
